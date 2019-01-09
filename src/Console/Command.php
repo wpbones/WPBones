8fb3093d85a70ce585cacbdeb1a40c5576a1f672
gfazioli <giovambattista.fazioli@gmail.com>
2019-01-09 11:09:48 +0100

    cosmetic

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -7,215 +7,214 @@ use WPKirk\WPBones\Support\Str;
 abstract class Command
 {
 
-  protected $signature;
+    protected $signature;
 
-  protected $options = [];
+    protected $options = [];
 
-  protected $description;
+    protected $description;
 
-  protected $argv = [];
+    protected $argv = [];
 
-  public $context;
+    public $context;
 
-  public $command;
+    public $command;
 
-  /**
-   * Display a colored line in the console.
-   *
-   * @param $str String to display.
-   */
-  protected function line( $str )
-  {
-    echo "\033[38;5;82m" . $str;
-    echo "\033[0m\n";
-  }
-
-  /**
-   * Display a colored line in the console.
-   *
-   * @param $str String to display.
-   */
-  protected function info( string $str )
-  {
-    echo "\033[38;5;213m" . $str;
-    echo "\033[0m\n";
-  }
-
-  /**
-   * Make a request to end user in the console and return the user input or the default value.
-   *
-   * @param string $str     The question string.
-   * @param string $default Optional. A default value whether the user press return.
-   *
-   * @return string
-   */
-  protected function ask( string $str, string $default = '' ) : string
-  {
-
-    echo "\n\e[38;5;88m$str" . ( empty( $default ) ? "" : " (default: {$default})" ) . "\e[0m ";
-
-    $handle = fopen( "php://stdin", "r" );
-    $line   = fgets( $handle );
-
-    fclose( $handle );
-
-    return trim( $line, " \n\r" );
-  }
-
-  public function __construct()
-  {
-    // get signature
-    $parts         = explode( ":", $this->signature, 2 );
-    $this->context = $parts[ 0 ];
-
-    // get command
-    if ( count( $parts ) > 1 ) {
-      $parts = explode( '{', $parts[ 1 ] );
-
-      // get options
-      if ( count( $parts ) > 1 ) {
-        $tempOptions = $parts;
-        array_shift( $tempOptions );
-        foreach ( $tempOptions as $optionInfo ) {
-          list( $option, $description ) = explode( ":", $optionInfo );
-
-          // sanitize
-          $option      = trim( $option );
-          $description = trim( rtrim( $description, "}" ) );
-
-          // check "=" params
-          if ( "=" == substr( $option, -1 ) ) {
-            $option                   = rtrim( $option, "=" );
-            $this->options[ $option ] = [ 'description' => $description, 'param' => true ];
-          }
-          else {
-            $this->options[ $option ] = [ 'description' => $description ];
-          }
-        }
-      }
-    }
-    $this->command = trim( ( $this->context . ":" . $parts[ 0 ] ) );
-  }
-
-  /**
-   * Return true if the options exists.
-   *
-   * @param string $value The option.
-   *
-   * @return bool|mixed
-   */
-  public function options( string $value )
-  {
-    $sanitizeOption = '--' . $value;
-
-    if ( ! in_array( $sanitizeOption, $this->argv ) ) {
-      return false;
+    /**
+     * Display a colored line in the console.
+     *
+     * @param $str String to display.
+     */
+    protected function line($str)
+    {
+        echo "\033[38;5;82m" . $str;
+        echo "\033[0m\n";
     }
 
-    if ( in_array( $sanitizeOption, array_keys( $this->options ) ) ) {
-      $option = $this->options[ $sanitizeOption ];
+    /**
+     * Display a colored line in the console.
+     *
+     * @param $str String to display.
+     */
+    protected function info(string $str)
+    {
+        echo "\033[38;5;213m" . $str;
+        echo "\033[0m\n";
+    }
 
-      if ( isset( $option[ 'param' ] ) && $option[ 'param' ] ) {
+    /**
+     * Make a request to end user in the console and return the user input or the default value.
+     *
+     * @param string $str     The question string.
+     * @param string $default Optional. A default value whether the user press return.
+     *
+     * @return string
+     */
+    protected function ask(string $str, string $default = ''): string
+    {
 
-        $argv = $this->argv;
+        echo "\n\e[38;5;88m$str" . (empty($default) ? "" : " (default: {$default})") . "\e[0m ";
 
-        foreach ( $argv as $argument ) {
-          if ( $argument == $sanitizeOption ) {
-            $valueParam = next( $argv );
-            break;
-          }
-          next( $argv );
+        $handle = fopen("php://stdin", "r");
+        $line   = fgets($handle);
+
+        fclose($handle);
+
+        return trim($line, " \n\r");
+    }
+
+    public function __construct()
+    {
+        // get signature
+        $parts         = explode(":", $this->signature, 2);
+        $this->context = $parts[0];
+
+        // get command
+        if (count($parts) > 1) {
+            $parts = explode('{', $parts[1]);
+
+            // get options
+            if (count($parts) > 1) {
+                $tempOptions = $parts;
+                array_shift($tempOptions);
+                foreach ($tempOptions as $optionInfo) {
+                    list($option, $description) = explode(":", $optionInfo);
+
+                    // sanitize
+                    $option      = trim($option);
+                    $description = trim(rtrim($description, "}"));
+
+                    // check "=" params
+                    if ("=" == substr($option, -1)) {
+                        $option                 = rtrim($option, "=");
+                        $this->options[$option] = ['description' => $description, 'param' => true];
+                    } else {
+                        $this->options[$option] = ['description' => $description];
+                    }
+                }
+            }
         }
+        $this->command = trim(($this->context . ":" . $parts[0]));
+    }
 
-        if ( ! isset( $valueParam ) || empty( $valueParam ) ) {
-            $this->info( 'Missing param' );
+    /**
+     * Return true if the options exists.
+     *
+     * @param string $value The option.
+     *
+     * @return bool|mixed
+     */
+    public function options(string $value)
+    {
+        $sanitizeOption = '--' . $value;
+
+        if (!in_array($sanitizeOption, $this->argv)) {
             return false;
         }
 
-        return $valueParam;
-      }
+        if (in_array($sanitizeOption, array_keys($this->options))) {
+            $option = $this->options[$sanitizeOption];
 
-      return true;
+            if (isset($option['param']) && $option['param']) {
+
+                $argv = $this->argv;
+
+                foreach ($argv as $argument) {
+                    if ($argument == $sanitizeOption) {
+                        $valueParam = next($argv);
+                        break;
+                    }
+                    next($argv);
+                }
+
+                if (!isset($valueParam) || empty($valueParam)) {
+                    $this->info('Missing param');
+
+                    return false;
+                }
+
+                return $valueParam;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
-    return false;
-  }
+    /**
+     * Display the help well formatted.
+     *
+     */
+    public function displayHelp()
+    {
+        $this->info("Usage:");
+        $this->line("  " . $this->command . " [options]");
+        $this->info("\nOptions:");
 
-  /**
-   * Display the help well formatted.
-   *
-   */
-  public function displayHelp()
-  {
-    $this->info( "Usage:" );
-    $this->line( "  " . $this->command . " [options]" );
-    $this->info( "\nOptions:" );
+        foreach ($this->options as $key => $value) {
+            $column2 = $value['description'];
+            if (isset($value['param'])) {
+                $column1 = $key . "[=value]";
+            } else {
+                $column1 = $key;
+            }
 
-    foreach ( $this->options as $key => $value ) {
-      $column2 = $value[ 'description' ];
-      if ( isset( $value[ 'param' ] ) ) {
-        $column1 = $key . "[=value]";
-      }
-      else {
-        $column1 = $key;
-      }
+            $column1 = $column1 . str_repeat(" ", (22 - strlen($column1)));
 
-      $column1 = $column1 . str_repeat( " ", ( 22 - strlen( $column1 ) ) );
-
-      $this->line( "  {$column1} {$column2}" );
-    }
-  }
-
-  /**
-   * Return the description of command console.
-   *
-   * @return string
-   */
-  public function getDescriptionAttribute() : string
-  {
-    return $this->description;
-  }
-
-  /**
-   * Set the argv.
-   *
-   * @param mixed $value Usually a string.
-   */
-  public function setArgvAttribute( $value )
-  {
-    $this->argv = $value;
-  }
-
-  /**
-   * Return the value of the method `get{Name}Attribute`.
-   *
-   * @param string $name Usually the protected property name.
-   *
-   * @return mixed
-   */
-  public function __get( string $name )
-  {
-    $method = 'get' . Str::studly( $name ) . 'Attribute';
-    if ( method_exists( $this, $method ) ) {
-      return $this->{$method}();
+            $this->line("  {$column1} {$column2}");
+        }
     }
 
-    return null;
-  }
-
-  /**
-   * Set the value of the method `set{Name}Attribute`.
-   *
-   * @param string $name Usually the protected property name.
-   * @param mixed  $value
-   */
-  public function __set( $name, $value )
-  {
-    $method = 'set' . Str::studly( $name ) . 'Attribute';
-    if ( method_exists( $this, $method ) ) {
-        $this->{$method}( $value );
+    /**
+     * Return the description of command console.
+     *
+     * @return string
+     */
+    public function getDescriptionAttribute(): string
+    {
+        return $this->description;
     }
-  }
 
-  abstract public function handle();
+    /**
+     * Set the argv.
+     *
+     * @param mixed $value Usually a string.
+     */
+    public function setArgvAttribute($value)
+    {
+        $this->argv = $value;
+    }
+
+    /**
+     * Return the value of the method `get{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     *
+     * @return mixed
+     */
+    public function __get(string $name)
+    {
+        $method = 'get' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            return $this->{$method}();
+        }
+
+        return null;
+    }
+
+    /**
+     * Set the value of the method `set{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     * @param mixed  $value
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            $this->{$method}($value);
+        }
+    }
+
+    abstract public function handle();
 }
--- src/Database/Seeder.php
+++ src/Database/Seeder.php
@@ -5,49 +5,49 @@ namespace WPKirk\WPBones\Database;
 class Seeder
 {
 
-  protected $tablename = null;
+    protected $tablename = null;
 
-  public function __construct()
-  {
-    $this->run();
+    public function __construct()
+    {
+        $this->run();
 
-  }
-
-  public function run()
-  {
-    // you have to override this method
-  }
-
-  protected function query( $sql )
-  {
-    global $wpdb;
-
-    return $wpdb->query( $sql );
-  }
-
-  protected function insert( $sql )
-  {
-    global $wpdb;
-
-    $tablename = $wpdb->prefix . $this->tablename;
-
-    return $wpdb->query( "INSERT INTO `{$tablename}` {$sql}" );
-  }
-
-  protected function truncate( $tablename = "" )
-  {
-    global $wpdb;
-
-    if ( empty( $tablename ) && is_null( $this->tablename ) ) {
-      return null;
     }
 
-    if ( empty( $tablename ) ) {
-      $tablename = $this->tablename;
+    public function run()
+    {
+        // you have to override this method
     }
 
-    $tablename = $wpdb->prefix . $tablename;
+    protected function query($sql)
+    {
+        global $wpdb;
 
-    return $wpdb->query( "TRUNCATE TABLE {$tablename}" );
-  }
+        return $wpdb->query($sql);
+    }
+
+    protected function insert($sql)
+    {
+        global $wpdb;
+
+        $tablename = $wpdb->prefix . $this->tablename;
+
+        return $wpdb->query("INSERT INTO `{$tablename}` {$sql}");
+    }
+
+    protected function truncate($tablename = "")
+    {
+        global $wpdb;
+
+        if (empty($tablename) && is_null($this->tablename)) {
+            return null;
+        }
+
+        if (empty($tablename)) {
+            $tablename = $this->tablename;
+        }
+
+        $tablename = $wpdb->prefix . $tablename;
+
+        return $wpdb->query("TRUNCATE TABLE {$tablename}");
+    }
 }
