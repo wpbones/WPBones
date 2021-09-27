a85c0d47ab8afd1c90c2300e364544950863e5d8
gfazioli <giovambattista.fazioli@gmail.com>
2021-09-27 18:13:39 +0200

    adds HasAttributes trait

---
+++ src/Support/Traits/HasAttributes.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace WPKirk\WPBones\Support\Traits;
+
+use WPKirk\WPBones\Support\Str;
+
+trait HasAttributes
+{
+    /**
+     * Return the value of the method `get{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     *
+     * @return mixed
+     */
+    public function __get(string $name)
+    {
+        $method = 'get' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            return $this->{$method}();
+        }
+
+        return null;
+    }
+
+    /**
+     * Set the value of the method `set{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     * @param mixed  $value
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            $this->{$method}($value);
+        }
+    }
+
+    /**
+     * Extend the __isset method to check for the method `get{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     */
+    public function __isset($name)
+    {
+        $method = 'isset' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            $this->{$method}($value);
+        }
+    }
+
+    /**
+     * Extend the __unset method to check for the method `get{Name}Attribute`.
+     *
+     * @param string $name Usually the protected property name.
+     */
+    public function __unset($name)
+    {
+        $method = 'unset' . Str::studly($name) . 'Attribute';
+        if (method_exists($this, $method)) {
+            $this->{$method}($value);
+        }
+    }
+
+    /**
+     * Determine if a get mutator exists for an attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasGetMutator($key)
+    {
+        return method_exists($this, 'get' . Str::studly($key) . 'Attribute');
+    }
+
+    /**
+     * Get the value of an attribute using its mutator.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    protected function mutateAttribute($key)
+    {
+        return $this->{'get' . Str::studly($key) . 'Attribute'}();
+    }
+
+    /**
+     * Determine if a set mutator exists for an attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasSetMutator($key)
+    {
+        return method_exists($this, 'set' . Str::studly($key) . 'Attribute');
+    }
+
+    /**
+     * Set the value of an attribute using its mutator.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function setMutatedAttributeValue($key, $value)
+    {
+        return $this->{'set' . Str::studly($key) . 'Attribute'}($value);
+    }
+}
