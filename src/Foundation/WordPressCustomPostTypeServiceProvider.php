3172006840f54c8d014a3ee501c7e3977d0615d4
Manuel Donini <ManuDoni@users.noreply.github.com>
2022-12-09 13:16:54 +0100

    Update WordPressCustomPostTypeServiceProvider.php
    Add missing custom post type args: "show_in_menu".
    It was initialized in the class but not used in the custom post type registration

--- src/Foundation/WordPressCustomPostTypeServiceProvider.php
+++ src/Foundation/WordPressCustomPostTypeServiceProvider.php
@@ -324,6 +324,7 @@ abstract class WordPressCustomPostTypeServiceProvider extends ServiceProvider
       'exclude_from_search'  => $this->excludeFromSearch,
       'publicly_queryable'   => $this->publiclyQueryable,
       'show_ui'              => $this->showUI,
+      'show_in_menu'         => $this->showInMenu,
       'show_in_nav_menus'    => $this->showInNavMenus,
       'show_in_admin_bar'    => $this->showInAdminBar,
       'menu_position'        => $this->menuPosition,
