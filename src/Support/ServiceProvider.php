f0fe5bae8771534260c2652017debdcfb0105976
gfazioli <giovambattista.fazioli@gmail.com>
2024-07-10 12:17:36 +0200

    ✨ adds mapPropertiesToArray() utility

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -35,4 +35,46 @@ abstract class ServiceProvider
       return;
     }
   }
+
+  /**
+   * Return an array of key => value properties.
+   *
+   * @param array $properties
+   *
+   * @example
+   *
+   * ```php
+   * $properties = [
+   *  // menu_icon will be the array key
+   *  // menuIcon is the property or a method of this class. Must be not null
+   *   'menu_icon' => 'menuIcon',
+   * ];
+   * ```
+   *
+   *
+   * @return array
+   */
+  protected function mapPropertiesToArray($properties): array
+  {
+    $result = [];
+
+    foreach ($properties as $key => $prop) {
+
+      if ($prop === 'register') {
+        throw new \Exception('The property name "register" is reserved.');
+      }
+
+      if (method_exists($this, $prop)) {
+        $value = $this->$prop();
+      } elseif (property_exists($this, $prop)) {
+        $value = $this->$prop;
+      }
+
+      if (!is_null($value)) {
+        $result[$key] = $value;
+      }
+    }
+
+    return $result;
+  }
 }
