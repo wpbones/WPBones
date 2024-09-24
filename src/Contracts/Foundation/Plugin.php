078a3efef373ecc3d44456d12f2b72f470e76504
gfazioli <giovambattista.fazioli@gmail.com>
2024-09-24 11:12:22 +0200

    🏷️ removes getBasePath() from the Plugin container

--- src/Contracts/Foundation/Plugin.php
+++ src/Contracts/Foundation/Plugin.php
@@ -4,12 +4,4 @@ namespace WPKirk\WPBones\Contracts\Foundation;
 
 use WPKirk\WPBones\Contracts\Container\Container;
 
-interface Plugin extends Container
-{
-  /**
-   * Get the base path of the Plugin installation.
-   *
-   * @return string
-   */
-  public function getBasePath(): string;
-}
+interface Plugin extends Container {}
